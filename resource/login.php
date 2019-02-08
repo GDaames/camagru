a7fd6eef7a4f0bda12c37a3e917f0126d526d046
GDaames <36263678+GDaames@users.noreply.github.com>
2019-02-08 06:05:52 +0200

    Add files via upload

--- resource/login.php
+++ resource/login.php
@@ -1,13 +1,12 @@
 <?php
-session_start();
+    include_once "../config/Database.php";
 
-try{
-    $con = new PDO ("mysql:host=localhost;dbname=camagru","root","123456");
+    session_start();
     if(isset($_POST['loginBtn'])){
         $email = ($_POST['email']);      // Turn our post into a local variable
         $pass = hash('whirlpool',$_POST['password']); 
 
-    $select = $con->prepare("SELECT * FROM users WHERE email='$email' and password='$pass'");
+    $select = $db->prepare("SELECT * FROM users WHERE email='$email' and password='$pass' and isVerified='1'");
     $select->setFetchMode(PDO::FETCH_ASSOC);
     $select->execute();
     $data=$select->fetch();
@@ -15,8 +14,5 @@ try{
         $_SESSION['password']=$data['password'];
     header("location: ../home.php");
     }
-}catch(PDOException $e)
-    {
-        echo "error".$e->getMessage();
-    }
 ?>
+
--- resource/signup.php
+++ resource/signup.php
@@ -1,39 +1,40 @@
 <?php
-    session_start();
+session_start();
 try{
     $con = new PDO("mysql:host=localhost;dbname=camagru","root","123456");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if(isset($_POST['signupBtn'])){
-
-        if(isset($_POST['email']) && !empty($_POST['email']) AND isset($_POST['password']) && !empty($_POST['password'])){
-            $name = ($_POST['email']);          // Turn our post into a local variable
-            $pass = ($_POST['password']);       // Turn our post into a local variable
-        }
-
         $username = $_POST['username'];
         $email = $_POST['email'];
-        $pass = $_POST['password'];
-        
-        $code = rand(100000, 999999); //$hash
-        $pass_hash = hash('whirlpool', $pass);
-        $insert = $con->prepare("INSERT INTO users (username,email,password,code)
-        VALUES (:username,:email,:password,:code)");
-            $insert->bindParam(':username',$username);
-            $insert->bindParam(':email',$email);
-            $insert->bindParam(':password',$pass_hash);
-            
-            $insert->bindParam(':code',$code); 
-            $insert->execute();
-            $str = "your verification link is http://localhost:8080/camagru/confirm.php?user=".$email."&code=".$code ;
-            $headers = 'From:noreply@camagru.com' . "\r\n"; // Set from headers
-            mail($email, "CAMAGRU Confirmation", $str, $headers);
-            // Return Success - Valid Email $msg = 'Your account has been made, <br /> please verify it by clicking the activation link that has been send to your email.';
-            echo "Link sent: You will be redirected in 5 seconds";
-            header('Refresh: 5; URL=http://localhost:8080/camagru/index.php');
+        $password = $_POST['password'];
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            echo("$email is a valid email address\n");
+        } else {
+            echo("$email is not a valid email address\n");
+            header('Refresh: 1; URL=http://localhost:8080/camagru/signup.php');
+        } 
+        if (strlen($password) < 6 || strlen($password) > 12 || !preg_match("#[A-Z]+#", $password)){
+            echo ("password must be 6-12 characters and contain atleast 1 capital letter\n");
+            header('Refresh: 3; URL=http://localhost:8080/camagru/signup.php');
+        } else {
+            $code = rand(100000, 999999);
+            $pass_hash = hash('whirlpool', $password);
+            $insert = $con->prepare("INSERT INTO users (username,email,password,code)
+            values(:username,:email,:password,:code)");
+                $insert->bindParam(':username',$username);
+                $insert->bindParam(':email',$email);
+                $insert->bindParam(':password',$pass_hash);
+                $insert->bindParam(':code',$code); 
+                $insert->execute();
+                $str = "your verification link is http://localhost:8080/camagru/confirm.php?user=".$email."&code=".$code ;
+                $headers = 'From:noreply@camagru.com' . "\r\n"; // Set from headers
+                mail($email, "CAMAGRU Confirmation", $str, $headers);
+                echo "Link sent: check your mail for a verification link.";
+                header('Refresh: 3; URL=http://localhost:8080/camagru/login.php');
         }
     }
-catch(PDOException $e)
-    {
+}catch(PDOException $e){
     echo "error".$e->getMessage();
-    }
+}
 ?>
